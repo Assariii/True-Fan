d45ff00627fbed5bf6f82aa5d8cbd16835c84370
Assari Yumyuang <a-s-rock12@hotmail.com>
2024-02-29 12:17:22 +0700

    Initial Commit

---
+++ True Fan/True_FanApp.swift
@@ -0,0 +1,17 @@
+//
+//  True_FanApp.swift
+//  True Fan
+//
+//  Created by Assari Yumyuang on 29/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct True_FanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
